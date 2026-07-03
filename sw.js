bd65d61917a36cb2df802eb0a3be3f22f031d35d
清貴 佐藤 <kiyosato1014@icloud.com>
2026-07-03 20:41:24 +0900

    BELLE HOUSE 点呼システム Ver2

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-const CACHE_NAME = "tenko-app-v1-gps-20260703-1";
-const ASSETS = ["./","index.html","manifest.json","icon-192.png","icon-512.png"];
-self.addEventListener("install", e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))).then(() => self.clients.claim()));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+const CACHE_NAME="belle-house-tenko-v2-20260703-1";const ASSETS=["./","index.html","manifest.json","icon-192.png","icon-512.png"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))).then(()=>self.clients.claim()))});self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
