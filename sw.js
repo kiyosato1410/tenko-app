60f8222897c80d310de9421fe16006477efb5e53
清貴 佐藤 <kiyosato1014@icloud.com>
2026-07-03 10:53:39 +0900

    キャッシュ更新

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
 self.addEventListener("install", event => {
-    self.skipWaiting();
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  clients.claim();
 });
 
 self.addEventListener("fetch", event => {
