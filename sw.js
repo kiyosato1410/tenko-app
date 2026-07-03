ad364c0de8ecfc1ac628d6899f5c5fab75740d38
清貴 佐藤 <kiyosato1014@icloud.com>
2026-07-03 21:17:03 +0900

    完成版

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const CACHE_NAME="belle-house-tenko-v2-fixed-20260703-2";self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE_NAME = "belle-house-tenko-final-20260703-4";
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
