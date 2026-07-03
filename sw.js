06ea9a8bb51fe150780d2e74802083330dceb24f
清貴 佐藤 <kiyosato1014@icloud.com>
2026-07-03 15:54:55 +0900

    完成版に復旧

--- sw.js
+++ sw.js
@@ -1,2 +1,12 @@
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+const CACHE_NAME = "tenko-app-v1-restore-20260703-2";
+const ASSETS = ["./","index.html","manifest.json","icon-192.png","icon-512.png"];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
